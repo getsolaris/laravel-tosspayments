14936de10d3ee0f04c92a5aefbd1f8838de9cb89
getsolaris <getsolaris.kr@gmail.com>
2022-11-06 12:36:54 +0900

    현금영수증 (CashReceipt) 구현

--- README.md
+++ README.md
@@ -259,6 +259,60 @@ $settlement = TossPayments::for(Settlement::class)
 return $settlement->json();
 ```
 
+
+
+## [현금영수증 (CashReceipt)](https://docs.tosspayments.com/reference#%ED%98%84%EA%B8%88%EC%98%81%EC%88%98%EC%A6%9D)
+
+### [현금영수증 발급](https://docs.tosspayments.com/reference#%ED%98%84%EA%B8%88%EC%98%81%EC%88%98%EC%A6%9D)
+
+GET /v1/cash-receipts
+
+```php
+use Getsolaris\LaravelTossPayments\TossPayments;
+use Getsolaris\LaravelTossPayments\Attributes\CashReceipt;
+
+$cashReceipt = TossPayments::for(CashReceipt::class)
+    ->amount($amount)
+    ->orderId($orderId)
+    ->orderName($orderName)
+    ->customerIdentityNumber($customerIdentityNumber)
+    ->type($type)
+    ->request();
+
+return $cashReceipt->json();
+```
+
+### [현금영수증 발급 취소](https://docs.tosspayments.com/reference#%ED%98%84%EA%B8%88%EC%98%81%EC%88%98%EC%A6%9D-%EB%B0%9C%EA%B8%89-%EC%B7%A8%EC%86%8C)
+
+POST /v1/cash-receipts/{receiptKey}/cancel
+
+```php
+use Getsolaris\LaravelTossPayments\TossPayments;
+use Getsolaris\LaravelTossPayments\Attributes\CashReceipt;
+
+$cashReceipt = TossPayments::for(CashReceipt::class)
+    ->receiptKey($receiptKey)
+    ->cancel();
+
+return $cashReceipt->json();
+```
+
+### [현금영수증 조회](https://docs.tosspayments.com/reference#%ED%98%84%EA%B8%88%EC%98%81%EC%88%98%EC%A6%9D-%EC%A1%B0%ED%9A%8C)
+
+GET /v1/cash-receipts
+
+```php
+use Getsolaris\LaravelTossPayments\TossPayments;
+use Getsolaris\LaravelTossPayments\Attributes\CashReceipt;
+
+$cashReceipts = TossPayments::for(CashReceipt::class)
+    ->requestDate($requestDate)
+    ->get();
+
+return $cashReceipts->json();
+```
+
+
 ## [카드사 혜택 조회 (CardPromotion)](https://docs.tosspayments.com/reference#%EC%B9%B4%EB%93%9C%EC%82%AC-%ED%98%9C%ED%83%9D-%EC%A1%B0%ED%9A%8C)
 
 ### [카드사 혜택 조회](https://docs.tosspayments.com/reference#%EC%B9%B4%EB%93%9C%EC%82%AC-%ED%98%9C%ED%83%9D-%EC%A1%B0%ED%9A%8C-1)
---
+++ src/Attributes/CashReceipt.php
@@ -0,0 +1,211 @@
+<?php
+
+namespace Getsolaris\LaravelTossPayments\Attributes;
+
+use Getsolaris\LaravelTossPayments\Contracts\AttributeInterface;
+use Getsolaris\LaravelTossPayments\Objects\Vbv;
+use Getsolaris\LaravelTossPayments\TossPayments;
+use GuzzleHttp\Promise\PromiseInterface;
+use Illuminate\Http\Client\Response;
+
+class CashReceipt extends TossPayments implements AttributeInterface
+{
+    /**
+     * @var string
+     */
+    protected string $uri;
+
+    /**
+     * @var string
+     */
+    protected string $receiptKey;
+
+    /**
+     * @var int
+     */
+    protected int $amount;
+
+    /**
+     * @var string
+     */
+    protected string $orderId;
+
+    /**
+     * @var string
+     */
+    protected string $orderName;
+
+    /**
+     * @var string
+     */
+    protected string $customerIdentityNumber;
+
+    /**
+     * @var string
+     */
+    protected string $type;
+
+    /**
+     * @var string
+     */
+    protected string $requestDate;
+
+    public function __construct()
+    {
+        parent::__construct($this);
+        $this->initializeUri();
+    }
+
+    /**
+     * @return $this
+     */
+    public function initializeUri(): static
+    {
+        $this->uri = '/cash-receipts';
+
+        return $this;
+    }
+
+    /**
+     * @param  string|null  $endpoint
+     * @param  bool  $withUri
+     * @return string
+     */
+    public function createEndpoint(?string $endpoint, bool $withUri = true): string
+    {
+        if ($withUri) {
+            return $this->url.$this->uri.$this->start($endpoint);
+        }
+
+        return $this->url.$this->start($endpoint);
+    }
+
+    /**
+     * @param  string  $receiptKey
+     * @return $this
+     */
+    public function receiptKey(string $receiptKey): static
+    {
+        $this->receiptKey = $receiptKey;
+
+        return $this;
+    }
+
+    /**
+     * @param  int  $amount
+     * @return $this
+     */
+    public function amount(int $amount): static
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    /**
+     * @param  string  $orderId
+     * @return $this
+     */
+    public function orderId(string $orderId): static
+    {
+        $this->orderId = $orderId;
+
+        return $this;
+    }
+
+    /**
+     * @param  string  $orderName
+     * @return $this
+     */
+    public function orderName(string $orderName): static
+    {
+        $this->orderName = $orderName;
+
+        return $this;
+    }
+
+    /**
+     * @param  string  $customerIdentityNumber
+     * @return $this
+     */
+    public function customerIdentityNumber(string $customerIdentityNumber): static
+    {
+        $this->customerIdentityNumber = $customerIdentityNumber;
+
+        return $this;
+    }
+
+    /**
+     * @param  string  $type
+     * @return $this
+     */
+    public function type(string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * @param  int|null  $taxFreeAmount
+     * @return PromiseInterface|Response
+     */
+    public function request(?int $taxFreeAmount = null): PromiseInterface|Response {
+        $parameters = [];
+        if ($taxFreeAmount) {
+            $parameters['taxFreeAmount'] = $taxFreeAmount;
+        }
+
+        return $this->client->post($this->createEndpoint('/'), [
+            'amount' => $this->amount,
+            'orderId' => $this->orderId,
+            'orderName' => $this->orderName,
+            'customerIdentityNumber' => $this->customerIdentityNumber,
+            'type' => $this->type,
+        ] + $parameters);
+    }
+
+    /**
+     * @param  int|null  $amount
+     * @return PromiseInterface|Response
+     */
+    public function cancel(?int $amount = null): PromiseInterface|Response {
+        $parameters = [];
+        if ($amount) {
+            $parameters['amount'] = $amount;
+        }
+
+        return $this->client->post($this->createEndpoint('/'.$this->receiptKey.'/cancel'), $parameters);
+    }
+
+    /**
+     * @param  string  $requestDate
+     * @return $this
+     */
+    public function requestDate(string $requestDate): static
+    {
+        $this->requestDate = $requestDate;
+
+        return $this;
+    }
+
+    /**
+     * @param  int|null  $cursor
+     * @param  int|null  $limit
+     * @return PromiseInterface|Response
+     */
+    public function get(?int $cursor = null, ?int $limit = null): PromiseInterface|Response {
+        $parameters = [];
+        if ($cursor) {
+            $parameters['cursor'] = $cursor;
+        }
+
+        if ($limit) {
+            $parameters['limit'] = $limit;
+        }
+
+        return $this->client->get($this->createEndpoint('/'), [
+            'requestDate' => $this->requestDate,
+        ] + $parameters);
+    }
+}
