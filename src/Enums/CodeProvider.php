2bd35a5a5c0b7893bd28d890e6ad9b2266b1d6d6
getsolaris <getsolaris.kr@gmail.com>
2022-12-05 22:24:17 +0900

    CodeProvider 클래스를 직접 호출하는 경우 예외처리

--- src/Enums/CodeProvider.php
+++ src/Enums/CodeProvider.php
@@ -3,6 +3,7 @@
 namespace Getsolaris\LaravelTossPayments\Enums;
 
 use Getsolaris\LaravelTossPayments\Exceptions\InvalidInputTargetCodeException;
+use LogicException;
 
 class CodeProvider
 {
@@ -13,16 +14,20 @@ class CodeProvider
     public static function toCode(int|string $code)
     {
         $class = get_called_class();
-        if (defined($class.'::'.$code)) {
-            return constant($class.'::'.$code)['code'];
-        }
+        if ($class === self::class) {
+            throw new LogicException(get_called_class().' 클래스를 직접 호출할 수 없습니다.');
+        } else {
+            if (defined($class.'::'.$code)) {
+                return constant($class.'::'.$code)['code'];
+            }
 
-        $constants = (new \ReflectionClass($class))->getConstants();
-        foreach ($constants as $constant) {
-            if ($constant['code'] === $code) {
-                return $constant['code'];
-            } elseif ($constant['kr'] === $code) {
-                return $constant['code'];
+            $constants = (new \ReflectionClass($class))->getConstants();
+            foreach ($constants as $constant) {
+                if ($constant['code'] === $code) {
+                    return $constant['code'];
+                } elseif ($constant['kr'] === $code) {
+                    return $constant['code'];
+                }
             }
         }
 
---
+++ tests/CodeProviderTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Getsolaris\LaravelTossPayments\tests;
+
+use Getsolaris\LaravelTossPayments\Enums\BankCode;
+use Getsolaris\LaravelTossPayments\Enums\CodeProvider;
+use Getsolaris\LaravelTossPayments\Exceptions\InvalidInputTargetCodeException;
+use LogicException;
+use PHPUnit\Framework\TestCase;
+
+class CodeProviderTest extends TestCase
+{
+    const TEST_TOSSBANK_CODE = 92;
+    const TEST_TOSSBANK_KR = '토스';
+
+    public function testCodeProvider(): void
+    {
+        $this->expectException(LogicException::class);
+        CodeProvider::toCode(self::TEST_TOSSBANK_CODE);
+    }
+}
