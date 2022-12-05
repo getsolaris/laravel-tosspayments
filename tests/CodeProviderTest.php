2d94e05fcb3721bb0d574ec317baa99c025af54b
getsolaris <getsolaris.kr@gmail.com>
2022-12-05 22:25:23 +0900

    CodeProviderTest 주석

--- tests/CodeProviderTest.php
+++ tests/CodeProviderTest.php
@@ -13,6 +13,12 @@ class CodeProviderTest extends TestCase
     const TEST_TOSSBANK_CODE = 92;
     const TEST_TOSSBANK_KR = '토스';
 
+    /**
+     * CodeProvider 클래스를 직접 호출하는 경우 예외처리
+     *
+     * @throws \ReflectionException
+     * @throws InvalidInputTargetCodeException
+     */
     public function testCodeProvider(): void
     {
         $this->expectException(LogicException::class);
