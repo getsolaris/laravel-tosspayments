fa8c64ebc41c3b831399a27486af87acc9b0b233
getsolaris <getsolaris.kr@gmail.com>
2022-12-06 13:24:56 +0900

    else 문 제거

--- src/Enums/CodeProvider.php
+++ src/Enums/CodeProvider.php
@@ -19,18 +19,18 @@ class CodeProvider
         $class = get_called_class();
         if ($class === self::class) {
             throw new LogicException(get_called_class().' 클래스를 직접 호출할 수 없습니다.');
-        } else {
-            if (defined($class.'::'.$code)) {
-                return constant($class.'::'.$code)['code'];
-            }
+        }
 
-            $constants = (new \ReflectionClass($class))->getConstants();
-            foreach ($constants as $constant) {
-                if ($constant['code'] === $code) {
-                    return $constant['code'];
-                } elseif ($constant['kr'] === $code) {
-                    return $constant['code'];
-                }
+        if (defined($class.'::'.$code)) {
+            return constant($class.'::'.$code)['code'];
+        }
+
+        $constants = (new \ReflectionClass($class))->getConstants();
+        foreach ($constants as $constant) {
+            if ($constant['code'] === $code) {
+                return $constant['code'];
+            } elseif ($constant['kr'] === $code) {
+                return $constant['code'];
             }
         }
 
